11ae828b2a6800ecf0e60cdad6b32501dc0d2b0b
Diego Azcarategui <diego.azcarategui@gmail.com>
2025-09-25 13:48:26 -0300

    test

---
+++ src/data/productos.js
@@ -0,0 +1,41 @@
+const productos = [
+  {
+    codigo: 'choco',
+    nombre: 'Torta de Chocolate',
+    categoria: 'Tortas',
+    descripcion: 'Deliciosa torta artesanal de chocolate.',
+    stock: 12,
+    precio: 15900,
+    imagen: '/img/pastelChocolate.png',
+  },
+  {
+    codigo: 'limon',
+    nombre: 'Pie de Limón',
+    categoria: 'Pies',
+    descripcion: 'Fresco pie de limón con merengue.',
+    stock: 8,
+    precio: 12900,
+    imagen: '/img/pastelTresLeches.png',
+  },
+  {
+    codigo: 'tvegana',
+    nombre: 'Torta Vegana',
+    categoria: 'Tortas Veganas',
+    descripcion: 'Rica torta sin origen animal.',
+    stock: 2,
+    precio: 18900,
+    imagen: '/img/tortaVegana.png',
+  },
+  {
+    codigo: 'vainilla',
+    nombre: 'Torta de Vainilla',
+    categoria: 'Tortas',
+    descripcion: 'Rica torta de Vainilla. ',
+    stock: 4,
+    precio: 14900,
+    imagen: '/img/tortaVainilla.png',
+  },
+ 
+];
+
+export default productos;
---
+++ src/pages/Login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+
+  return (
+    <div>
+      <h1>Esta página sera para ingresar User y Password</h1>
+    </div>
+  );
+}
